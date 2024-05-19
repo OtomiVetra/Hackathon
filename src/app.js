c2d0a2f6ba7fe9b6a55f32f2f8d7e0c06eaaa2f6
Alexsey Sinyakin <lexavandal@mail.ru>
2024-05-19 19:21:54 +0300

    master

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import './styles.css'
+import { ContextMenu } from './menu'
+import { ShapeModule } from './modules/shape.module'
+import { ClicksModule } from './modules/clicks.module' 
+import { BackgroundModule } from './modules/background.module'
 
 
 
+const menu = new ContextMenu('.menu')
+const backgroundModule = new BackgroundModule('asd', 'asd')
+const shape = new ShapeModule('shape', 'shape') 
+const clicks = new ClicksModule('click' ,'click')  
 
+
+menu.open()
+menu.close()
+
+
+menu.add(backgroundModule)
+menu.add(clicks)
+menu.add(shape)
